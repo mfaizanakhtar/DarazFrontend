13a7df34df5b5edb2c0fbe69888f06fa8c1a6a01
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-06-30 22:01:43 +0500

    update

--- src/app/orders-view/orders-view.component.ts
+++ src/app/orders-view/orders-view.component.ts
@@ -9,77 +9,123 @@ import { OrdersService } from '../services/orders.service';
   styleUrls: ['./orders-view.component.css']
 })
 export class OrdersViewComponent implements OnInit {
-  orders:any
+  //data
+  orders:any;
+  StoreArray=[]
+  todayDate=new Date();
+  backDate=new Date();
+  //filters
   Fulfillment='Dropshipping';
-  Store='all';
-  StatusFilter='all';
-  StoreArray=[];
-  raworders:any;
-  filtered:any;
+  Store="All";
+  StatusFilter=null;
+  enddate:Date
+  startdate:Date
+  OrderId=null
+  TrackingCode=null
+  //datatable variables
   ColumnMode = ColumnMode;
   loadingIndicator = false;
-  reorderable = true;
   SelectionType = SelectionType;
   selected=[];
-  temp=[];
   status:any
   //for Indexing
   pSize=10
   pIndex=0
-  //forselection
+  length:number;
+  //forStatusDefaultselection
   selectedVal
 
   //paginator
-pageEvent: PageEvent;
-datasource: null;
-pageIndex:number;
-pageSize:number;
-length:number;
+  pageEvent: PageEvent;
   @ViewChild(DatatableComponent) table: DatatableComponent;
 
   constructor(private orderService:OrdersService) { }
 
   ngOnInit(): void {
-    this.getOrders(this.pSize,this.pIndex,'')
-    this.selectedVal='all'
+    this.selectedVal='All'
+    this.backDate.setDate(this.backDate.getDate()-15)
+    this.enddate=this.todayDate
+    this.startdate=this.backDate 
+    this.getOrders()
   }
 
-  getOrders(pageSize,pageNumber,OrderId){
-    console.log(OrderId)
-    this.pSize=pageSize;
-    this.pIndex=pageNumber;
+  getOrders(){  
+    if(this.StatusFilter=='All') this.StatusFilter=null
+    if(this.Store=='All') this.Store=null
+    if(this.Fulfillment=='All') this.Fulfillment=null
+
     this.loadingIndicator = true;
-    this.orderService.get('/'+this.StatusFilter+'?'+'pageSize='+pageSize+"&pageNumber="+pageNumber+OrderId).subscribe(res=>{
+    this.orderService.get('/?'+'OrderItems.Status='+this.StatusFilter+'&pageSize='+this.pSize+"&pageNumber="+this.pIndex
+    +"&OrderId="+this.OrderId+"&OrderItems.TrackingCode="+this.TrackingCode+"&ShopId="+this.Store+"&OrderItems.ShippingType="+this.Fulfillment+"&startDate="+this.startdate.toISOString()+"&endDate="+this.enddate.toISOString()).subscribe(res=>{
       console.log(res)
       this.orders=res[0]
       this.length=res[1]
+      this.StoreArray=res[2]
       this.loadingIndicator = false;
     })
   }
 
   StatusFilterClicked(status){
+    this.pSize=10
+    this.pIndex=0
     this.StatusFilter=status
-    this.getOrders(this.pSize,this.pIndex,'')
+    this.getOrders()
+  }
+
+  DateInput(mode,event){
+    if(mode == 'start'){
+      this.startdate = event.value
+    }
+    if(mode == 'end'){
+      if(event.value != null){
+        this.enddate = event.value
+        this.pSize=10
+        this.pIndex=0
+        this.getOrders()
+      }
+    }
+    
+
   }
 
   formatDate(date){
     var d = new Date(date);
-    var formattedDate=d.toLocaleDateString('en-US',{weekday:'long'})+' '+ d.getDate()+'-'+d.getMonth()+'-'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes()
+    var month = d.getMonth()+1
+    var formattedDate=d.toLocaleDateString('en-US',{weekday:'long'})+' '+ d.getDate()+'-'+month+'-'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes()
     return formattedDate
   }
 
-  getServerData(event?:PageEvent){
-    console.log(event)
-    this.getOrders(event.pageSize,event.pageIndex,'')
+  changePageData(event?:PageEvent){
+    this.pSize=event.pageSize;
+    this.pIndex=event.pageIndex
+    this.getOrders()
   }
 
   findOrder(f){
-    console.log(f.value.OrderId)
-    if(f.value.OrderId=='' || f.value.OrderId==null){
-    this.getOrders(10,0,'')
+    // console.log(f.value)
+    this.OrderId=f.value.OrderId
+    this.TrackingCode=f.value.Tracking
 
-    }
-    else this.getOrders(10,0,"&OrderId="+f.value.OrderId)
+    if(this.OrderId=="") this.OrderId=null
+    if(this.TrackingCode=="") this.TrackingCode=null
+
+    this.getOrders()
+  }
+
+  FulfillmentSelected(event){
+    this.pSize=10
+    this.pIndex=0
+    this.Fulfillment = event.value
+
+    this.getOrders()
+  }
+  StoreSelected(event){
+    this.pSize=10
+    this.pIndex=0
+    if(event.value=="All") this.Store=null
+    else this.Store = event.value
+
+    this.getOrders()
   }
 //NGX-DATATABLE
   onSelect({ selected }) {
