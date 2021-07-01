f38866c6d57c3279bccd924ba0f790ec0ff5241e
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-07-01 21:55:52 +0500

    update

--- src/app/print-labels/print-labels.component.ts
+++ src/app/print-labels/print-labels.component.ts
@@ -1,5 +1,5 @@
 import { LabelService } from './../services/label.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {jsPDF} from 'jspdf'
 import html2canvas from 'html2canvas'
 
@@ -11,21 +11,10 @@ import html2canvas from 'html2canvas'
 export class PrintLabelsComponent implements OnInit {
   orders:any
   constructor(private labels:LabelService) { }
-
+  
   ngOnInit(): void {
     this.orders=this.labels.getOrders()
     console.log(this.orders)
-    // this.print()
   }
-
-  // print(){
-  //   const printContent = document.getElementById("pdfLabels");
-  //   const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
-  //   WindowPrt.document.write(printContent.innerHTML);
-  //   WindowPrt.document.close();
-  //   WindowPrt.focus();
-  //   WindowPrt.print();
-  //   // WindowPrt.close();
-  // }
 
 }
