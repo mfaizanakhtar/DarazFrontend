dc3367ec166f449dc77d1aa56df4560edd9af4cf
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-07-02 19:18:09 +0500

    update

--- src/app/print-labels/print-labels.component.ts
+++ src/app/print-labels/print-labels.component.ts
@@ -2,6 +2,7 @@ import { LabelService } from './../services/label.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {jsPDF} from 'jspdf'
 import html2canvas from 'html2canvas'
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-print-labels',
@@ -10,11 +11,17 @@ import html2canvas from 'html2canvas'
 })
 export class PrintLabelsComponent implements OnInit {
   orders:any
-  constructor(private labels:LabelService) { }
+  constructor(private labels:LabelService,private sanitizer:DomSanitizer) { }
   
   ngOnInit(): void {
     this.orders=this.labels.getOrders()
     console.log(this.orders)
   }
 
+  getImage(string){
+   var imagePath = this.sanitizer.bypassSecurityTrustResourceUrl(string);
+    return imagePath
+  }
 }
+
+
