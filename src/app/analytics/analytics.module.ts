247fda26b12fdbe031bfc0b7e16f509430fd2abd
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-10-01 21:48:13 +0500

    update

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -4,12 +4,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { LoginGuard } from '../LoginGuard';
+import { AnalyticsDashboardComponent } from './analytics-dashboard/analytics-dashboard.component';
+import { AnalyticsContentComponent } from './analytics-content/analytics-content.component';
+import { StatComponent } from './stat/stat.component';
+import { HorizontalComponent } from './horizontal/horizontal.component';
+
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    AnalyticsDashboardComponent,
+    AnalyticsContentComponent,
+    StatComponent,
+    HorizontalComponent
   ],
   imports: [
     SharedModule,
@@ -19,6 +28,10 @@ import { LoginGuard } from '../LoginGuard';
         component:DashboardComponent,
         canActivate:[LoginGuard]
       },
+      {
+        path:'analytics',
+        component:HorizontalComponent,
+      }
     ])
   ],
   providers:[LoginGuard]
